66d966c87b79fcd70fd6e49f65e858e1d17222ef
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-02-15 18:47:57 -0500

    Create FAQ Collapse project in Day 12

---
+++ day_12/app.js
@@ -0,0 +1,7 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.parentNode.classList.toggle('active');
+  });
+});
