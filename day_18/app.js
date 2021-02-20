1e04de64573edca55d0fefd7abe2e92eb1252f70
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-02-20 14:10:49 -0500

    Create the Background Slider project in Day 18

---
+++ day_18/app.js
@@ -0,0 +1,40 @@
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+
+let activeSlide = 0;
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove('active'));
+
+  slides[activeSlide].classList.add('active');
+}
+
+right.addEventListener('click', () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+left.addEventListener('click', () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
